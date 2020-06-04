cb639503c5c75a71b06fa62e9ce5adb77647400f
Mariana Coelho <coelhomariana@outlook.com>
2020-06-04 13:01:20 -0300

    estrutura do projeto

---
+++ src/components/CreateNewPlaylist/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TextField from '@material-ui/core/Textfield';
+import Button from '@material-ui/core/Button';
+
+class CreateNewPLaylist extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            newPlaylistName: ""
+        }
+    }
+
+    handleNameInputChange = (event) => {
+        this.setState({
+            newPlaylistName: event.target.value
+        })
+    }
+
+    render(){
+        
+        return(
+            <div>
+                <TextField
+                    type="text"
+                    onChange={this.handleNameInputChange}
+                    value={this.state.newPlaylistName}
+                />
+                <Button>Criar playlist</Button>
+            </div>
+        )
+    }
+}
+
+export default CreateNewPLaylist;
